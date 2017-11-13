36bfd7e34965003d8b6baf7068755f9320668ea5
Sam Gutentag <sam@gutentag.co>
2017-11-13 11:42:10 -0800

    bike share program steamer, collects data from 5 bike share programs

--- GBFS_streamer.py
+++ GBFS_streamer.py
@@ -5,25 +5,20 @@ import os
 from math import ceil, floor
 import sched, time
 
+
 import pandas as pd
 
 
+def get_stream_json(feed_url="https://gbfs.fordgobike.com/gbfs/en/station_status.json",
+                        time_zone='America/Los_Angeles',
+                        time_adjustment=pd.Timedelta('00:00:00'),
+                        time_adjust_forward=True):
 
-
-# cogo_system_feeds_url = 'https://gbfs.cogobikeshare.com/gbfs/gbfs.json'
-# cogo_system_information_feed_url = "https://gbfs.cogobikeshare.com/gbfs/en/system_information.json"
-# cogo_station_information_feed_url = "https://gbfs.cogobikeshare.com/gbfs/en/station_information.json"
-# cogo_station_status_feed_url = "https://gbfs.cogobikeshare.com/gbfs/en/station_status.json"
-
-
-def get_stream_json(feed_url):
+    # make json request across network
     with urllib.request.urlopen(feed_url) as url:
         url_json_response = json.loads(url.read().decode())
 
-
     print('\tLast Updated\t%s' % datetime.fromtimestamp(url_json_response['last_updated']).strftime("%A, %B %d, %Y %H:%M:%S"))
-
-    refresh_timer = int(url_json_response['ttl'])
 
     df = pd.DataFrame(url_json_response['data'])
     df.head()
@@ -31,79 +26,169 @@ def get_stream_json(feed_url):
     df['last_reported'] = pd.to_datetime(df['last_reported'],unit='s')
 
 
-    # Correct timezone
-    df['last_reported'] = df['last_reported'] - pd.Timedelta('05:00:00')
+    # correct time_zone
+    if time_adjust_forward:
+        df['last_reported'] = df['last_reported'] + time_adjustment
+    else:
+        df['last_reported'] = df['last_reported'] - time_adjustment
+
     df.set_index('last_reported', inplace=True)
-    df.tz_localize('America/New_York')
+    df.tz_localize(time_zone)
     df.reset_index(inplace=True)
 
-    return (df, refresh_timer)
+    return df
 
-def df_to_csv(df, filepath):
+def df_to_csv(df, file_dir, file_path):
 
-    if os.path.exists(filepath):
+    # make sure directory exists
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+
+    if os.path.exists(file_path):
         # append if already exists
-        with open(filepath, 'a') as f:
+        with open(file_path, 'a') as f:
             df.to_csv(f, index=False, header=False)
     else:
         # make a new file if not
-        with open(filepath, 'w') as f:
+        with open(file_path, 'w') as f:
             df.to_csv(f, index=False, header=True)
 
-
-def stream_gbfs_feed_json_to_csv(feed_url, feed_service='SERVICE', feed_name='FEED'):
-
-    df, refresh_timer = get_stream_json(feed_url)
-
-    if refresh_timer < 10:
-        refresh_timer = 15
-    else:
-        refresh_timer += 5
+    return True
 
 
-    # Timestamp file
+def stream_gbfs_feed_json_to_csv(feed_url="https://gbfs.fordgobike.com/gbfs/en/station_status.json",
+                                    feed_service='babs',
+                                    feed_name='system_status',
+                                    time_zone='America/Los_Angeles',
+                                    time_adjustment=pd.Timedelta('00:00:00'),
+                                    time_adjust_forward = True,
+                                    file_interval = 30):
+
+    df = get_stream_json(feed_url=feed_url, time_zone=time_zone, time_adjustment=time_adjustment, time_adjust_forward=time_adjust_forward)
+
+    # Timestamp file - down to ten minute intervals
     dt = datetime.now()
-    m = floor(dt.minute/5)*5
-    tstamp = '{:04d}{:02d}{:02d}{:02d}{:02d}'.format(dt.year, dt.month, dt.day, dt.hour, dt.minute)
-    file_name = 'streamed_data/%s/station_status_data_%s.csv' % (feed_service.lower(), tstamp)
-    file_name = 'streamed_data/%s/%s_%s_data_%s.csv' % (feed_service.lower(), feed_service.lower(), feed_name.lower(), tstamp)
-    df_to_csv(df, file_name)
+    m = floor(dt.minute/file_interval)*file_interval
+    tstamp = '{:04d}{:02d}{:02d}{:02d}{:02d}'.format(dt.year, dt.month, dt.day, dt.hour, m)
 
-    return refresh_timer
+    feed_service = feed_service.replace('_', '')
 
+    file_dir = 'streamed_data/%s/' % (feed_service.lower())
+    file_name = 'streamed_data/%s/%s_%s_data_%s.csv' % (feed_service.lower(),
+                                                            feed_service.lower(),
+                                                            feed_name.lower(),
+                                                            tstamp)
+    df_to_csv(df, file_dir, file_name)
 
-
+    return True
 
 def main():
 
     s = sched.scheduler(time.time, time.sleep)
-    def do_something(sc):
+    def do_something(sc,):
 
         cogo_feed_url = "https://gbfs.cogobikeshare.com/gbfs/en/station_status.json"
-        babs_feed_url = "https://gbfs.fordgobike.com/gbfs/en/station_status.json"
-
-        feed_url = cogo_feed_url
-        program_name = 'cogo'
-        feed_name = 'station_status'
-
-
-        print('[%s] - Fetching feed from \'%s\'...' % (datetime.now().time(), feed_url))
+        print('[%s] - Fetching feed from \'%s\'...' % (datetime.now().time(), cogo_feed_url))
         try:
-            refresh_timer = stream_gbfs_feed_json_to_csv(feed_url, feed_service=program_name, feed_name=feed_name)
+            stream_gbfs_feed_json_to_csv(cogo_feed_url,
+                                            feed_service='cogo',
+                                            feed_name=cogo_feed_url.split('/')[-1][:-5],
+                                            time_zone = 'America/New_York',
+                                            time_adjustment=pd.Timedelta('5:00:00'),
+                                            time_adjust_forward=False)
         except:
-            print('---- Somethign went wrong, moving on ----')
+            print('---- Something went wrong, moving on ----')
+
+
+        babs_feed_url = "https://gbfs.fordgobike.com/gbfs/en/station_status.json"
+        print('[%s] - Fetching feed from \'%s\'...' % (datetime.now().time(), babs_feed_url))
+        try:
+            stream_gbfs_feed_json_to_csv(babs_feed_url,
+                                            feed_service='babs',
+                                            feed_name=babs_feed_url.split('/')[-1][:-5],
+                                            time_zone = 'America/Los_Angeles',
+                                            time_adjustment=pd.Timedelta('8:00:00'),
+                                            time_adjust_forward=False)
+        except:
+            print('---- Something went wrong, moving on ----')
+
+        # Newy York City
+        citybike_feed_url = "https://gbfs.citibikenyc.com/gbfs/en/station_status.json"
+        print('[%s] - Fetching feed from \'%s\'...' % (datetime.now().time(), babs_feed_url))
+        try:
+            stream_gbfs_feed_json_to_csv(babs_feed_url,
+                                            feed_service='citibike',
+                                            feed_name=citybike_feed_url.split('/')[-1][:-5],
+                                            time_zone = 'America/New_York',
+                                            time_adjustment=pd.Timedelta('5:00:00'),
+                                            time_adjust_forward=False)
+        except:
+            print('---- Something went wrong, moving on ----')
+
+
+        # Boston, MA
+        hubway_feed_url = "https://gbfs.thehubway.com/gbfs/en/station_status.json"
+        print('[%s] - Fetching feed from \'%s\'...' % (datetime.now().time(), babs_feed_url))
+        try:
+            stream_gbfs_feed_json_to_csv(babs_feed_url,
+                                            feed_service='hubway',
+                                            feed_name=hubway_feed_url.split('/')[-1][:-5],
+                                            time_zone = 'America/New_York',
+                                            time_adjustment=pd.Timedelta('5:00:00'),
+                                            time_adjust_forward=False)
+        except:
+            print('---- Something went wrong, moving on ----')
+
+        # Washington, D.C.
+        cabi_feed_url = "https://gbfs.capitalbikeshare.com/gbfs/en/station_status.json"
+        print('[%s] - Fetching feed from \'%s\'...' % (datetime.now().time(), babs_feed_url))
+        try:
+            stream_gbfs_feed_json_to_csv(babs_feed_url,
+                                            feed_service='divvy',
+                                            feed_name=cabi_feed_url.split('/')[-1][:-5],
+                                            time_zone = 'America/New_York',
+                                            time_adjustment=pd.Timedelta('5:00:00'),
+                                            time_adjust_forward=False)
+        except:
+            print('---- Something went wrong, moving on ----')
+
+        # Chicago, IL
+        divvy_feed_url = "https://gbfs.divvybikes.com/gbfs/en/station_status.json"
+        print('[%s] - Fetching feed from \'%s\'...' % (datetime.now().time(), babs_feed_url))
+        try:
+            stream_gbfs_feed_json_to_csv(babs_feed_url,
+                                            feed_service='divvy',
+                                            feed_name=divvy_feed_url.split('/')[-1][:-5],
+                                            time_zone = 'America/Chicago',
+                                            time_adjustment=pd.Timedelta('6:00:00'),
+                                            time_adjust_forward=False)
+        except:
+            print('---- Something went wrong, moving on ----')
+
+
+
+
+
+
+
+
         seconds = 60
-        minutes = 3
-        refresh_timer = minutes*seconds
-        print('\t[%s] - Waiting %s minutes to run again...\n' % (datetime.now().time(), minutes))
-        s.enter(refresh_timer, 1, do_something, (sc,))
+        interval = INTERVAL
+        print('\t[%s] - Waiting %s minutes to run again...\n' % (datetime.now().time(), interval))
+        s.enter(interval*seconds, 1, do_something, (sc,))
 
 
     s.enter(0, 1, do_something, (s,))
     s.run()
 
+    return True
+
 
 
 
 if __name__ == '__main__':
+
+    global INTERVAL
+    INTERVAL = 5
+
     main()
